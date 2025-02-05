6b2e9a9f65064ae15a84058052f2187017c33cdb
SofianeZemraniIUT <sofiane.zemrani@iut-dijon.u-bourgogne.fr>
2025-02-05 18:47:34 +0100

    Debut des test pour le pokemoncard

--- tests/pokemonCard.test.ts
+++ tests/pokemonCard.test.ts
@@ -5,7 +5,113 @@ import { prismaMock } from './jest.setup';
 describe('PokemonCard API', () => {
   describe('GET /pokemon-cards', () => {
     it('should fetch all PokemonCards', async () => {
-      const mockPokemonCards = [];
+      const mockPokemonCards = [
+        {
+          "id": 1,
+          "name": "Charmander",
+          "pokedexId": 4,
+          "lifePoints": 39,
+          "size": 0.6,
+          "weight": 8.5,
+          "imageUrl": "charmander",
+          "typeId": 2
+        },
+        {
+          "id": 2,
+          "name": "Squirtle",
+          "pokedexId": 7,
+          "lifePoints": 44,
+          "size": 0.5,
+          "weight": 9,
+          "imageUrl": "squirtle",
+          "typeId": 3
+        },
+        {
+          "id": 3,
+          "name": "Bulbasaur",
+          "pokedexId": 1,
+          "lifePoints": 45,
+          "size": 0.7,
+          "weight": 6.9,
+          "imageUrl": "bulbasaur",
+          "typeId": 4
+        },
+        {
+          "id": 4,
+          "name": "Jigglypuff",
+          "pokedexId": 39,
+          "lifePoints": 115,
+          "size": 0.5,
+          "weight": 5.5,
+          "imageUrl": "jigglypuff",
+          "typeId": 1
+        },
+        {
+          "id": 5,
+          "name": "Gengar",
+          "pokedexId": 94,
+          "lifePoints": 60,
+          "size": 1.5,
+          "weight": 40.5,
+          "imageUrl": "gengar",
+          "typeId": 14
+        },
+        {
+          "id": 6,
+          "name": "Eevee",
+          "pokedexId": 133,
+          "lifePoints": 55,
+          "size": 0.3,
+          "weight": 6.5,
+          "imageUrl": "eevee",
+          "typeId": 1
+        },
+        {
+          "id": 7,
+          "name": "Snorlax",
+          "pokedexId": 143,
+          "lifePoints": 160,
+          "size": 2.1,
+          "weight": 460,
+          "imageUrl": "snorlax",
+          "typeId": 1
+        },
+        {
+          "id": 8,
+          "name": "Dragonite",
+          "pokedexId": 149,
+          "lifePoints": 91,
+          "size": 2.2,
+          "weight": 210,
+          "imageUrl": "dragonite",
+          "typeId": 15
+        },
+        {
+          "id": 9,
+          "name": "Mewtwo",
+          "pokedexId": 150,
+          "lifePoints": 106,
+          "size": 2,
+          "weight": 122,
+          "imageUrl": "mewtwo",
+          "typeId": 11
+        },
+        {
+          "id": 10,
+          "name": "Bulbizarre 214",
+          "pokedexId": 549,
+          "lifePoints": 45,
+          "size": null,
+          "weight": null,
+          "imageUrl": null,
+          "typeId": 1
+        }
+      ];
+
+
+
+
+      const response = await request(app).get('/pokemons-cards');
 
       expect(response.status).toBe(200);
       expect(response.body).toEqual(mockPokemonCards);
@@ -14,8 +120,18 @@ describe('PokemonCard API', () => {
 
   describe('GET /pokemon-cards/:pokemonCardId', () => {
     it('should fetch a PokemonCard by ID', async () => {
-      const mockPokemonCard = {};
+      const mockPokemonCard = {
+        "id": 5,
+        "name": "Gengar",
+        "pokedexId": 94,
+        "lifePoints": 60,
+        "size": 1.5,
+        "weight": 40.5,
+        "imageUrl": "gengar",
+        "typeId": 14
+      };
 
+      const response = await request(app).get('/pokemons-cards/5');
       expect(response.status).toBe(200);
       expect(response.body).toEqual(mockPokemonCard);
     });
